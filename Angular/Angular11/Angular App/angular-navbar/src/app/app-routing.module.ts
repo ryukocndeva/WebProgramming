6d12e16a57bd9bbaa646606ca1de81863f32503f
ryukocndeva@gmail.com <ryukocndeva@gmail.com>
2020-12-26 22:48:36 -0800

    Finished angular-navbar project.

--- Angular/Angular11/Angular App/angular-navbar/src/app/app-routing.module.ts
+++ Angular/Angular11/Angular App/angular-navbar/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: DummyComponent},
+  { path: 'services', component: DummyComponent},
+  { path: 'projects', component: DummyComponent},
+  { path: 'aboutus', component: DummyComponent},
+  { path: 'signup', component: DummyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular/Angular11/Angular App/angular-navbar/src/app/app.module.ts
+++ Angular/Angular11/Angular App/angular-navbar/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'; 
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DummyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule,
+    AppRoutingModule,
+    BrowserAnimationsModule, 
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
