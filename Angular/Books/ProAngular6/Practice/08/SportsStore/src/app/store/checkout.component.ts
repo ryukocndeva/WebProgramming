d9aaafc4ebf828fabc490ded396f3bb34509c710
ryukocndeva <ryukocndeva@gmail.com>
2021-01-02 01:57:04 -0800

    Added routing.

---
+++ Angular/Books/ProAngular6/Practice/08/SportsStore/src/app/store/checkout.component.ts
@@ -0,0 +1,14 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "cart-detail",
+    templateUrl: "cartDetail.component.html"
+})
+export class CheckoutComponent { }
+
+
+
+
+
+
