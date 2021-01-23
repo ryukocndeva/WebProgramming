01d74a7725e77b495adb552cb5e1c89c97cfed2c
Developer <dev@OS20210114X32Git1.test.dev>
2021-01-23 17:03:14 +0000

    Access to database.

--- Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/trydjango/urls.py
+++ Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/trydjango/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, contact_view, about_view, social_view
+from products.views import product_detail_view
 
 urlpatterns = [
     path('', home_view, name='home'),
@@ -24,5 +25,6 @@ urlpatterns = [
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('social/', social_view, name='social'),
+    path('product/', product_detail_view, name='product_detail'),
     path('admin/', admin.site.urls),
 ]
