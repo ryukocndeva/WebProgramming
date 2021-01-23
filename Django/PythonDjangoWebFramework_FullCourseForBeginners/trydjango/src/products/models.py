6abe4999862755b42568774f8f2941dee488c812
Developer <dev@OS20210114X32Git1.test.dev>
2021-01-23 14:28:58 +0000

    Before adding homepage.

--- Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/products/models.py
+++ Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/products/models.py
@@ -3,9 +3,10 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
 	title		=	models.CharField(max_length=120)
-	descripion	=	models.TextField(blank=True, null=True)
+	description	=	models.TextField(blank=True, null=True)
 	price		=	models.DecimalField(decimal_places=2, max_digits=10000)
-	summary		=	models.TextField()
+	summary		=	models.TextField(blank=True, null=False)
+	featured	=	models.BooleanField()
 
 	def __str__(self):
 		return self.title
