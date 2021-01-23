014b2acda49e9717df42f57c51418a28f72530ed
Developer <dev@OS20210114X32Git1.test.dev>
2021-01-23 16:46:48 +0000

    Added a loop & condition.

--- Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/pages/views.py
+++ Django/PythonDjangoWebFramework_FullCourseForBeginners/trydjango/src/pages/views.py
@@ -12,7 +12,14 @@ def contact_view(request, *args, **kwargs):
 
 def about_view(request, *args, **kwargs):
 	# return HttpResponse("<h1>About Page</h1>")
-	return render(request, "about.html", {})
+
+	my_context = {
+		"my_text" : "This is about us",
+		"my_number" : 123,
+		"my_list" : [123, 4242, 12345, "Abc"],
+	}
+
+	return render(request, "about.html", my_context)
 
 def social_view(request, *args, **kwargs):
 	# return HttpResponse("<h1>Social Page</h1>")
