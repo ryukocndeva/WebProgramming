9eb7c7a470250b99d8e97af5ffb1f32d570d57c2
Developer <dev@OS20210114X32Git1.test.dev>
2021-01-24 06:39:41 +0000

    Added rest_framework.

--- Django/django-todo-react/backend/backend/settings.py
+++ Django/django-todo-react/backend/backend/settings.py
@@ -39,10 +39,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'todo',
+	'corsheaders',							# Add
+	'rest_framework',						# Add
+	'todo',									# Add
 ]
 
 MIDDLEWARE = [
+	'corsheaders.middleware.CorsMiddleware', # Add
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Add
+CORS_ORIGIN_WHITELIST = [
+	'http://localhost:3000',
+]
+
+
+
